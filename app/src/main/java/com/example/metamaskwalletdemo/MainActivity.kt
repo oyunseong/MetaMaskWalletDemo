8b1e340f1955989574033a82078dd34886f20aad
gkrls <gkrlsanj1@gmail.com>
2023-04-02 01:02:17 +0900

    데이터바인딩 추가

--- app/src/main/java/com/example/metamaskwalletdemo/MainActivity.kt
+++ app/src/main/java/com/example/metamaskwalletdemo/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.metamaskwalletdemo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.metamaskwalletdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment)
 
