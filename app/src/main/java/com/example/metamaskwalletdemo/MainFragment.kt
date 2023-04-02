aa4007262e7ca604e270f0637a18d9f060a938ee
gkrls <gkrlsanj1@gmail.com>
2023-04-02 17:04:53 +0900

    Feat: 지갑 주소 클립보드에 복사 기능 추가
    메인화면 상단에 있는 지갑주소를 클릭하면 클립보드에 주소가 복사할 수 있게 기능을 추가함

--- app/src/main/java/com/example/metamaskwalletdemo/MainFragment.kt
+++ app/src/main/java/com/example/metamaskwalletdemo/MainFragment.kt
@@ -1,6 +1,53 @@
 package com.example.metamaskwalletdemo
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.metamaskwalletdemo.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        // 스왑 화면으로 이동
+        binding.swapButton.setOnClickListener {
+            findNavController().navigate(R.id.action_MainFragment_to_SwapFragment)
+        }
+
+        binding.walletAddress.setOnClickListener {
+            copyText(mainViewModel._wallet.value?.get(0)?.address.toString())
+            Toast.makeText(requireContext(), "토큰 주소가 복사되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun copyText(text: String) {
+        val clipBoard: ClipboardManager =
+            requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("text", text)
+        clipBoard.setPrimaryClip(clip)
+    }
+
 }
